558426b0ee3ea13ae2966eeb741932bff85c6849
costantino pacilio <costantinopacilio7@hotmail.it>
2026-06-25 14:47:54 +0200

    updated python version in setup

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ if __name__ == "__main__":
             "Intended Audience :: Science/Research",
             "License :: OSI Approved :: MIT License",
             "Natural Language :: English",
-            "Programming Language :: Python :: 3.8",
+            "Programming Language :: Python :: 3.9",
             "Topic :: Scientific/Engineering :: Physics",
             "Topic :: Scientific/Engineering :: Astronomy",
         ],
